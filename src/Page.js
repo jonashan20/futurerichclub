a7c504910e506987cb185ec103ec471918d791c8
KRUH102034\Administrator <batgirl1@naver.com>
2023-04-26 16:35:10 +0900

    no message

--- src/Page.js
+++ src/Page.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import questions from './question.json';
+import { useState} from 'react';
 
 function Page() {   
-  return (    
 
+  const [count, setCount] = useState(0);  //질문 진행 count  
+  const navigate  = useNavigate();
+
+  const handleBack = () =>  {   
+      if (!!questions.all[count-1]){
+        setCount(count-1)
+      } else {
+        navigate('/Main');     
+      } 
+	}
+
+  const handleNext = () =>  {   
+      if (!!questions.all[count+1]){
+        setCount(count+1)
+      } else {
+        navigate('/Result');     
+      }
+	}
+
+
+  return (    
+      
       <div className='ly-contents'>
 
         <div className='box-contents'>
 
-          <dl>
-            <dt>01</dt>
-            <dd>문동은이 사는 빌라 이름은?</dd>
-          </dl>
 
-          <div className='wrap-form'>
+          {/* 현재 Question 렌더링 */}        
+          <div><dl><dt>{questions.all[count].num}</dt><dd>{questions.all[count].question}</dd></dl></div>;      
+          
+          {/* 현재 카운트 질문에 대한 Choices 렌더링 */}        
+          { questions.all[count].choices.map((item) => {                                                
+              return <dl> <dd>{item.num}.{item.desc}</dd></dl>;              
+          })}
+        
+
+          {/* 주관식 처리는 추후  */}
+          {/* <div className='wrap-form'>
             <input className='input-basic' type='text' placeholder='4글자'></input>
             <p>( * 4글자 )</p>
-          </div>
-          <Link to='/Result'>
-            <button className='btn-basic'>다음으로</button>
-          </Link>
+          </div> */}
+
+          { count < 1 ? null: <button className='btn-basic' onClick={handleBack}>이전으로</button> }
+          {/* <Link to='/Result'> */}
+          <button className='btn-basic' onClick={handleNext}>다음으로</button>
+          {/* </Link> */}
+          <Link to='/Main'>
+            <button className='btn-basic'>다시하기</button>
+          </Link>  
         </div>
      
         <div className='bg-main'>
@@ -32,4 +66,4 @@ function Page() {
   );
 }
 
-export default Page;     // <= App이 위 함수 이름 & 파일이름과 같아야함
+export default Page;  
